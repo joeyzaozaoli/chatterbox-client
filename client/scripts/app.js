959212323fb881e71cdc8fcc4bce6080d6f38c5a
Joey Li <joeyzaozaoli@gmail.com>
2017-07-16 15:11:03 -0700

    Create fetch and send methods for HTTP requests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,57 @@
-// YOUR CODE HERE:
+var app = {};
+
+app.server = 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages';
+
+app.init = function() {
+  app.fetch();
+  setInterval(app.fetch, 10000);
+};
+
+app.fetch = function() {
+  $.ajax({
+    url: app.server,
+    type: 'GET',
+    success: function(data) {
+      console.log('get success', data);
+      app.clearMessages();
+      _.each(data.results, function(msgObj) {
+        app.renderMessage(msgObj);
+      });
+    },
+    error: function(data) {
+      console.log('get failure', data);
+    }
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+
+app.renderMessage = function(msgObj) {
+  $('#chats').append(`
+    <div>
+      <span>${msgObj.text}</span>
+      <span>#${msgObj.username}</span>
+      <span>@${msgObj.roomname}</span>
+    </div>
+  `);
+};
+
+app.send = function(msgObj) {
+  $.ajax({
+    url: app.server,
+    type: 'POST',
+    data: JSON.stringify(msgObj),
+    success: function() {
+      console.log('send success');
+    },
+    error: function() {
+      console.log('send failure');
+    }
+  });
+};
+
+$(document).ready(function() {
+  app.init();
+});
